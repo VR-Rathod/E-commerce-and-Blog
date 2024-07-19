b08ef849d6dc781b6c6b2f269b012e81adabbdac
VKing9 <124020182+VR-Rathod@users.noreply.github.com>
2024-07-19 17:29:05 +0530

    used DRY Price.. And code inheritence

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,12 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request , 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n =len(products)
+    nSlides = n//4 + ceil((n/4)- (n//4))
+    # params = {'no_of_slides': nSlides , 'range': range(1 , nSlides) , 'product': products}
+    allProdes = [[products , range(1 , len(products)), nSlides] , 
+                 [products , range(1 , len(products)), nSlides]
+                 ]
+    params = {'allProds': allProdes}
+    return render(request , 'shop/index.html' , params)
 
 def about(request):
-    return HttpResponse("This is about")
+    return render(request , 'shop/about.html')
 
 def contact(request):
     return HttpResponse("This is contact")
